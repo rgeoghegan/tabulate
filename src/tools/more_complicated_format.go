eeaf5096fa2818fbfef0fee1a1f66b5fa7199a05
Rory Geoghegan <r.geoghegan@gmail.com>
2016-05-29 18:07:07 -0400

    Tried with a more complicated setup.

---
+++ src/tools/more_complicated_format.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "fmt"
+    "tabulate"
+)
+
+type Bloom struct {
+    first string
+    last string
+}
+
+func (b *Bloom) String() string {
+    return fmt.Sprintf("%s %s", b.first, b.last)
+}
+
+type Blah struct {
+    Name *Bloom
+    Amount int
+    Location string
+    Done bool
+}
+
+func main() {
+    var blahs []*Blah
+
+    blahs = append(blahs, &Blah{&Bloom{"Roy", "Smith"}, 15, "Washington D.C.", true})
+    blahs = append(blahs, &Blah{&Bloom{"Fred", "Flanders"}, 100, "Montreal", false})
+    blahs = append(blahs, &Blah{&Bloom{"Bobby", "Smith"}, -2, "San Fransisco", false})
+    blahs = append(blahs, &Blah{&Bloom{"Jolene", "Lee"}, 234, "Guyene", true})
+
+    table, err := tabulate.Tabulate(blahs)
+    if err != nil {panic(err)}
+
+    fmt.Println(table)
+    fmt.Printf("Done!\n")
+}
